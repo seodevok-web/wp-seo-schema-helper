01c06d3a0137ec81680396420b6d133539efa4ef
Edu Nezzan <seodevok@gmail.com>
2026-01-22 00:23:23 +0700

    Add files via upload

---
+++ wp-seo-schema-helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: WP SEO Schema Helper
+ * Description: A lightweight helper plugin for adding basic SEO-related schema markup in WordPress.
+ * Version: 0.1.0
+ * Author: Edu Nezzan
+ * Author URI: https://nezzan.edu.eu.org
+ * License: GPL-2.0+
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('wp_head', function () {
+    if (is_singular()) {
+        ?>
+        <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "WebPage",
+          "name": "<?php echo esc_js(get_the_title()); ?>",
+          "url": "<?php echo esc_url(get_permalink()); ?>"
+        }
+        </script>
+        <?php
+    }
+});
